df0e7d4cf47381ea00d65991dcb8bdad6c90e972
RezhaBlue <rezaul_h@ymail.com>
2015-05-07 02:56:22 -0400

    deleted comment-outs and rearranged lines

--- audioPaint.m
+++ audioPaint.m
@@ -1,17 +1,18 @@
 function [resultImage] = audioPaint(filename)
-<<<<<<< HEAD
 	[soundWave, Fs] = audioread(filename);
-    %print soundWave
-    soundWave
+    soundSize = size(soundWave);
+    
+    monoSound = (soundWave(:,1) + soundWave(:,2))/2;
+    monoSize = size(monoSound);
     %get the discrete fourier transform using fft
-    dft = fft(soundWave);
-    freq = 0:Fs/length(soundWave):Fs/2;
+    dft = fft(monoSound);
+    freq = 0:Fs/length(monoSound):Fs/2;
     %cut the dft down to half length
-    dft = dft(1:length(soundWave)/2+1);
+    dft = dft(1:length(monoSound)/2+1);
     %plots the frequencies for the dft
-    %plot(freq,abs(dft))
+    plot(freq,abs(dft));
     %shows the peaks
-    psdest = psd(spectrum.periodogram,soundWave,'Fs',Fs,'NFFT',length(soundWave));
+    psdest = psd(spectrum.periodogram,monoSound,'Fs',Fs,'NFFT',length(soundWave));
     %plot(psdest)
     
 %{ 
@@ -21,17 +22,9 @@ wait xcoord should eb the time not the fft itself
 we would use the freq from fft to figure out the sprite we're using
 %}
     %find peaks in data
-    %[pks,locs,w,p] = findpeaks(dft)
+    %findpeaks(monoSound)
     
-=======
-    landscape = imread('landscape.png'),
-    imshow(landscape),
-	[soundWave, Fs] = audioread(filename),
-    soundSize = size(soundWave),
-    
-    monoSound = (soundWave(:,1) + soundWave(:,2))/2,
-    
-    
+    landscape = imread('landscape.png');
+    %imshow(landscape);
 
->>>>>>> origin/master
 end
