8497e851411d13dfe38a25870130a9fb08436afd
Philippe Kimura-Thollander <philippekimurathollander@gmail.com>
2015-05-07 17:57:32 -0400

    made overlaying image work

--- audioPaint.m
+++ audioPaint.m
@@ -26,15 +26,24 @@ we would use the freq from fft to figure out the sprite we're using
     %findpeaks(monoSound)
     %[pks,locs,w,p] = findpeaks(dft)
     
+    %replace these x and y values with dyanamic ones
+    x = 100; y = 100;
     landscape = imread('landscape.png');
     
     latias = imread('latias.png');
+    [im_x, im_y, dim] = size(latias);
+    
+    blend_im = zeros(im_x, im_y, dim, 'uint8');
+    patch1 = landscape(x+1:x+im_x, y+1:y+im_y, :);
+    patch2 = latias;
+    mask = (patch2 == 0);
+    blend_im = uint8(mask) .* patch1 + (1-uint8(mask)) .* patch2;
+    landscape(x+1:x+im_x, y+1:y+im_y,:) = blend_im;
+    imshow(landscape);
     
     %landscape(100, 50, latios);
     %imshow(landscape);
         
-    C = imfuse(landscape, latias,'blend','Scaling','joint');
-    imshow(C);
     %image sequencing for videos: http://www.mathworks.com/help/images/what-is-an-image-sequence.html
     %databasing images: http://www.mathworks.com/matlabcentral/answers/159725-how-to-create-images-database-in-matlab-to-store-5-images
     %overlaying: http://stackoverflow.com/questions/11486956/how-to-overlay-several-images-in-matlab
