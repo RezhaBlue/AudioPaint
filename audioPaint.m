2d3678bf2093aa90bd29b4065a80763d9f4125dd
Philippe Kimura-Thollander <philippekimurathollander@gmail.com>
2015-05-07 02:11:36 -0400

    added mono and background

--- audioPaint.m
+++ audioPaint.m
@@ -1,6 +1,11 @@
 function [resultImage] = audioPaint(filename)
+    landscape = imread('landscape.png'),
+    imshow(landscape),
 	[soundWave, Fs] = audioread(filename),
+    soundSize = size(soundWave),
     
-    print soundWave
+    monoSound = (soundWave(:,1) + soundWave(:,2))/2,
+    
+    
 
 end
